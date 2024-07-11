118eb52ac0cfb45b69c4ee9eb4f597c0315ca402
Taufiq Syed <tfqsyed@gmail.com>
2024-07-11 16:19:31 +0400

    Fixed minor validation bug

--- news_api/app/repositories.py
+++ news_api/app/repositories.py
@@ -5,7 +5,7 @@ class NewsArticleRepository:
     def getNewsArticles(params):
         try:
             params['apiKey'] = os.environ['NEWS_API_KEY']
-            r = requests.get('https://newsapi.org/v2/everything', params=params)
+            r = requests.get('https://newsapi.org/v2/top-headlines', params=params)
             if r.status_code == 200:
                 return r.json()['articles']
             return None
